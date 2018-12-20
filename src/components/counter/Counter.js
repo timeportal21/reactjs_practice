da86cac61a052d9d3337ae5aaf462d046752be07
Rabin shrestha <timeportal21@gmail.com>
2018-12-20 11:21:22 +0545

    only +ve while decrement

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -7,14 +7,27 @@ class Counter extends Component {
     };
 
     handleIncrement = () => {
-        this.setState({ counter: this.state.counter +1 });
+        this.setState({ 
+            counter: this.state.counter +1 
+        });
     };
+
+    handleDecrement = () => {
+        let counter = this.state.counter -1;
+        if(counter< 0){
+            counter = 0;
+        } 
+        this.setState({
+            counter: counter
+        })
+    }
 
     render() {
         return(
             <div>
                 <h1>Counter: {this.state.counter}</h1>
                 <button onClick={this.handleIncrement}>increment</button>
+                <button onClick={this.handleDecrement}>Decrement</button>
             </div>
         )
     }
